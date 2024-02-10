b708ce1757e130fc586d317e46d04096e3642468
amliwaw <amliwaw@yandex.ru>
2024-02-10 23:13:21 +0300

    add section characteristics interface

--- include/pe_library/section.hpp
+++ include/pe_library/section.hpp
@@ -24,13 +24,34 @@ namespace pe_library
 		void set_virtual_size(std::uint32_t value) noexcept;
 
 		void set_readable() noexcept;
+		void unset_readable() noexcept;
+		bool is_readable() const noexcept;
+
 		void set_writeable() noexcept;
+		void unset_writeable() noexcept;
+		bool is_writeable() const noexcept;
+
 		void set_executable() noexcept;
+		void unset_executable() noexcept;
+		bool is_executable() const noexcept;
+
+		void set_shared() noexcept;
+		void unset_shared() noexcept;
+		bool is_shared() const noexcept;
+
+		void set_discardable() noexcept;
+		void unset_discardable() noexcept;
+		bool is_discardable() const noexcept;
 
 	private:
 		std::string _name;
 		std::uint32_t _characteristics;
 		std::uint32_t _virtual_size;
+
+	private:
+		void set_characteristic(std::uint32_t value) noexcept;
+		void unset_characteristic(std::uint32_t value) noexcept;
+		bool has_characteristic(std::uint32_t value) const noexcept;
 	};
 }
 
